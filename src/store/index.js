c4dc89eadd9beaced6271126c77d190c6e62b934
Михалев Артем <mikhalevardm@gmail.com>
2022-06-29 10:32:51 +0300

    add app logic in vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,54 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: () => ({}),
-  mutations: {},
-  actions: {},
-  getters: {},
+  state: () => ({
+    unselectedFaculties: [],
+    selectedFaculties: [],
+    loadingStatus: false,
+  }),
+  mutations: {
+    addFaculties(state, data) {
+      state.unselectedFaculties = data;
+    },
+    selectFaculty(state, id) {
+      state.selectedFaculties.push(
+        ...state.unselectedFaculties.splice(
+          state.unselectedFaculties.findIndex((item) => {
+            return item.id === id;
+          }),
+          1
+        )
+      );
+    },
+    unselectFaculty(state, id) {
+      state.unselectedFaculties.push(
+        ...state.selectedFaculties.splice(
+          state.selectedFaculties.findIndex((item) => {
+            return item.id === id;
+          }),
+          1
+        )
+      );
+    },
+  },
+  actions: {
+    // fake api
+    async fetchFaculties({ commit, state }) {
+      state.loadingStatus = true;
+      let faculties = await import("@/faculties");
+      commit("addFaculties", faculties.default);
+      state.loadingStatus = false;
+    },
+  },
+  getters: {
+    getUnselectedFaculties(state) {
+      return state.unselectedFaculties;
+    },
+    getSelectedFaculties(state) {
+      return state.selectedFaculties;
+    },
+    getLoadingStatus(state) {
+      return state.loadingStatus;
+    },
+  },
 });

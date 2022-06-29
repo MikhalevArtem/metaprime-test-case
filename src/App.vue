bb6748bb08c8466585cc3977d10be9db9fa74650
Михалев Артем <mikhalevardm@gmail.com>
2022-06-29 10:38:10 +0300

    setup and styling App component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,32 @@
 <template>
-  <div>Hello</div>
+  <div
+    class="wrapper h-screen mx-auto pa-6 d-flex flex-column flex-sm-row justify-start justify-sm-space-around"
+  >
+    <unselected-faculties></unselected-faculties>
+    <selected-faculties></selected-faculties>
+  </div>
 </template>
 <script>
-export default {};
+import { mapActions } from "vuex";
+import UnselectedFaculties from "@/components/UnselectedFaculties.vue";
+import SelectedFaculties from "@/components/SelectedFaculties.vue";
+export default {
+  components: {
+    UnselectedFaculties,
+    SelectedFaculties,
+  },
+  methods: {
+    ...mapActions(["fetchFaculties"]),
+  },
+  mounted() {
+    this.fetchFaculties();
+  },
+};
 </script>
-<style scoped></style>
+<style scoped>
+.wrapper {
+  background: linear-gradient(90deg, #f598a8, #f6edb2);
+  max-width: 1200px;
+  gap: 20px;
+}
+</style>
